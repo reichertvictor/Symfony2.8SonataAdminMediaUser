9b63bbe93d4b2733cf348c5e02376a0cfddd1b76
Victor <Victor@Victor-PC>
2016-10-29 09:21:21 -0300

    Symfony 2.8 + Sonata Admin 3.9 + Sonata Media 3.3 + Sonata User 3.1

---
+++ app/autoload.php
@@ -0,0 +1,16 @@
+<?php
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Composer\Autoload\ClassLoader;
+
+/**
+ * @var ClassLoader $loader
+ */
+$loader = require __DIR__.'/../vendor/autoload.php';
+
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+//$loader->add('Html2Pdf_', __DIR__.'/../vendor/html2pdf/lib');
+$loader->add("Application", __DIR__.'/src/Application');
+$loader->register();
+
+return $loader;
